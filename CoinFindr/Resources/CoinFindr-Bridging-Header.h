952ec8176e62b5e2c300aa4cbb7032d0be27e4d9
Marsal Silveira <marsal.silveira@gmail.com>
2018-02-25 18:34:10 -0300

    Making project ready to start development  - configure Cocoapods with basic pods  - configure swift version to 4.0  - initial integration with travis CI  - gitignore

---
+++ CoinFindr/Resources/CoinFindr-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  CoinFindr-Bridging-Header.h
+//  CoinFindr
+//
+//  Created by Marsal Silveira.
+//  Copyright © 2018 Marsal Silveira. All rights reserved.
+//
+
+#ifndef CoinFindr_Bridging_Header_h
+#define CoinFindr_Bridging_Header_h
+
+//#import <GoogleMaps/GoogleMaps.h>
+
+#endif /* CoinFindr_Bridging_Header_h */
